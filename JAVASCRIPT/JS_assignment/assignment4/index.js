394e55f13798ed83ff715d9059842bb86bdcee45
rohit <duttarohit754@gmail.com>
2025-02-18 20:04:14 +0530

    Finished with some minor bug fixes

--- JAVASCRIPT/JS_assignment/assignment4/index.js
+++ JAVASCRIPT/JS_assignment/assignment4/index.js
@@ -7,12 +7,19 @@ const saveCartToLocalStorage = () => {
   localStorage.setItem("cart", JSON.stringify(cart));
 };
 
+
 const clearWholeCart = () => {
+  if (cart.length < 1) {
+    return showNotification("Cart already empty", "error");
+  }
   cart = [];
 
   localStorage.removeItem("cart");
   saveCartToLocalStorage()
   showNotification("🛒 Cart cleared!", "info");
+  updateCartUi();
+  document.querySelector(".total-price").innerText = `$${0}`;
+  return
 }
 
 const showNotification = (message, type = "info", duration = 3000) => {
@@ -36,6 +43,8 @@ const addToCart = (id) => {
     if (!cartedItem) {
       cart.push({ ...product, quantity: 1 });
       showNotification("Added to cart", "success");
+      saveCartToLocalStorage();
+      updateCartUi()
       return;
     } else {
       cartedItem.quantity++;
@@ -44,16 +53,37 @@ const addToCart = (id) => {
 
 
     saveCartToLocalStorage();
+    updateCartUi()
 
 
   }
   catch (e) {
     console.log(e);
     showNotification("⚠️ Failed to load products!", "error");
+    return;
 
   }
 }
 
+const removeFromCart = (id) => {
+  const cartedItem = cart.find(item => item.food_id === id);
+  if (cartedItem.quantity > 1) {
+    cartedItem.quantity--;
+    showNotification("Cart updated", "info");
+    saveCartToLocalStorage();
+    updateCartUi();
+    return;
+  }
+
+  cart = cart.filter(item => item.food_id !== id);
+  showNotification("Cart updated", "info");
+  saveCartToLocalStorage();
+  updateCartUi();
+  document.querySelector(".total-price").innerText = `$${0}`;
+  return;
+
+
+}
 
 
 
@@ -74,7 +104,7 @@ const getProducts = async () => {
               alt="food image">
             <div class="card-details">
               <p class="title">${item.food_name} <span>(${item.food_category})</span></p>
-              <p class="price">$${item.food_price}</p>
+              <p class="price">$${parseInt(item.food_price)}</p>
               <p class="description">${item.food_description}</p>
             </div>
             <div class="buttons">
@@ -92,11 +122,12 @@ const getProducts = async () => {
         addToCart(parseInt(e.target.dataset.id));
       });
     });
+    updateCartUi()
 
     return;
   } catch (e) {
     console.error("Error fetching data:", e);
-    alert(e.message);
+    showNotification("Internal server issue", "error");
   }
 };
 
@@ -109,6 +140,60 @@ document.querySelector(".clear-cart").addEventListener("click", () => {
 
 })
 
+const updateCartUi = () => {
+  let totalAmount = 0;
+  const cartContainer = document.querySelector(".cartItems");
+
+  if (cart.length < 1) {
+    cartContainer.innerHTML = `<p class="noItems">No Items selected</p>`;
+    return;
+  }
+
+
+  let tableHTML = `
+    <table  width="100%">
+      <thead>
+        <tr>
+          <th>Food Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  cart.forEach(item => {
+    tableHTML += `
+      <tr>
+        <td>${item.food_name}</td>
+        <td> <button data-id="${item.food_id}" class="removeItem">-</button> ${item.quantity}  <button data-id="${item.food_id}" class="increaseQuantity">+</button></td>
+        <td>$${item.food_price}</td>
+        <td>$${parseInt((item.food_price) * item.quantity)}</td>
+      </tr>
+    `;
+    totalAmount += item.food_price * item.quantity;
+  });
+
+  tableHTML += `
+      </tbody>
+    </table>
+  `;
+
+  cartContainer.innerHTML = tableHTML;
 
 
 
+  document.querySelector(".total-price").innerText = `$${parseInt(totalAmount)}`;
+  return
+};
+
+document.querySelector(".cartItems").addEventListener("click", (e) => {
+  if (e.target.classList.contains("removeItem")) {
+    removeFromCart(parseInt(e.target.dataset.id));
+  }
+
+  if (e.target.classList.contains("increaseQuantity")) {
+    addToCart(parseInt(e.target.dataset.id));
+  }
+});
